35059dff942d8bf66d36bdbfceb881376b97fcd1
hovvko <volodymyr.holovko13@gmail.com>
2021-11-12 02:11:30 +0200

    JS homework 1

---
+++ homework_1/homework_1.js
@@ -0,0 +1,248 @@
+// let hello = "hello";
+// let owu = "owu";
+// let com = "com";
+// let ua = "ua";
+// let one = 1;
+// let ten = 10;
+// let number = -999;
+// let number_2 = 123;
+// let number_float = 3.14;
+// let number_float_2 = 2.7;
+// let number_3 = 16;
+// let boolean_1 = false;
+// let boolean_2 = true;
+
+
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(one);
+// console.log(ten);
+// console.log(number);
+// console.log(number_2);
+// console.log(number_float);
+// console.log(number_float_2) ;
+// console.log(number_3);
+// console.log(boolean_1);
+// console.log(boolean_2);
+
+
+// document.write(`${hello}<br>`);
+// document.write(`${owu}<br>`);
+// document.write(`${com}<br>`);
+// document.write(`${ua}<br>`);
+// document.write(`${one}<br>`);
+// document.write(`${ten}<br>`);
+// document.write(`${number}<br>`);
+// document.write(`${number_2}<br>`);
+// document.write(`${number_float}<br>`);
+// document.write(`${number_float_2}<br>`) ;
+// document.write(`${number_3}<br>`);
+// document.write(`${boolean_1}<br>`);
+// document.write(`${boolean_2}<br>`);
+
+
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(number);
+// alert(number_2);
+// alert(number_float);
+// alert(number_float_2)
+// alert(number_3);
+// alert(boolean_1);
+// alert(boolean_2);
+
+
+// let hello = "привіт";
+// let owu = "ову";
+// let com = "ком";
+// let ua = "юа";
+// let one = 11;
+// let ten = 100;
+// let number = -9999;
+// let number_2 = 1234;
+// let number_float = 3.145;
+// let number_float_2 = 2.77;
+// let number_3 = 166;
+// let boolean_2 = true;
+// let boolean_1 = false;
+
+
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(one);
+// console.log(ten);
+// console.log(number);
+// console.log(number_2);
+// console.log(number_float);
+// console.log(number_float_2) ;
+// console.log(number_3);
+// console.log(boolean_1);
+// console.log(boolean_2);
+
+
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(number);
+// alert(number_2);
+// alert(number_float);
+// alert(number_float_2)
+// alert(number_3);
+// alert(boolean_1);
+// alert(boolean_2);
+
+
+// document.write(`${hello}<br>`);
+// document.write(`${owu}<br>`);
+// document.write(`${com}<br>`);
+// document.write(`${ua}<br>`);
+// document.write(`${one}<br>`);
+// document.write(`${ten}<br>`);
+// document.write(`${number}<br>`);
+// document.write(`${number_2}<br>`);
+// document.write(`${number_float}<br>`);
+// document.write(`${number_float_2}<br>`) ;
+// document.write(`${number_3}<br>`);
+// document.write(`${boolean_1}<br>`);
+// document.write(`${boolean_2}<br>`);
+
+
+// let firstName = "Володимир";
+// let middleName = "Степанович";
+// let lastName = "Головко";
+// let person = (`${firstName}  ${middleName}  ${lastName}`);
+//
+// console.log(person);
+
+
+// let userName = prompt('Write your name');
+// let userMiddleName = prompt('Write your middle name');
+// let userAge = prompt('Write your age');
+//
+// document.write(`Hello ${userName} ${userMiddleName} your age is ${userAge}`);
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+// console.log(5 < 6);
+// console.log(5 == 6);
+// console.log(5 > 6);
+// console.log(5 >= 6);
+//
+// console.log(10 == 10);
+// console.log(10 === 10);
+// console.log(10 !== 10);
+// console.log(10 > 10);
+// console.log(10 < 10);
+//
+// console.log(123 == '123');
+// console.log(123 === '123');
+
+
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); ///205
+// document.write(str - a + "<br/>"); ///15
+// document.write(str * "2" + "<br/>"); ///40
+// document.write(str / 2 + "<br/>");  ///10
+
+
+//CLASSWORK
+
+// let numbers = [11, 22, 33, 44, 55, 66, 77, 88, 99, 10]
+// console.log(
+//     numbers[0]
+//     + numbers[1]
+//     + numbers[2]
+//     + numbers[3]
+//     + numbers[4]
+//     + numbers[5]
+//     + numbers[6]
+//     + numbers[7]
+//     + numbers[8]
+//     + numbers[9]
+// )
+
+
+// let book = {
+//     name: "Таинственный остров",
+//     genre: "Фентази",
+//     number_of_pages: 480
+// }
+
+
+// let book = {
+//     name: "Восход Драконов",
+//     number_of_pages: 600,
+//     genre: "Фантастика, Фэнтези,Зарубежная литература",
+//     author: "Морган Райс"
+// }
+
+
+// let book = [
+//     {
+//         name: "Преступление и наказание",
+//         number_of_pages: 672,
+//         genre: "Роман, Криминальный Дектектив",
+//         author: "Федор Достоевский"
+//     },
+//
+//     {
+//         name: "Мастер и Маргарта",
+//         number_of_pages: 504,
+//         genre: "Литература Модернизма",
+//         author: "Михаил Булгаков"
+//     },
+//
+//     {
+//         name: "Восход Доблести",
+//         number_of_pages: 590,
+//         genre: "Фантастика, Фэнтези",
+//         author: "Морган Райс"
+//     }
+// ]
+//
+//
+// console.log(book[0])
+// console.log(book[1])
+// console.log(book[2])
+
+
+// let height = 23
+// let width = 10
+// s = height * width
+// console.log(s)
+
+
+// let heightC = 2
+// let diameterC = 1.2
+// let radius = diameterC / 2
+// let pr = radius**2 * 3.14
+// let v = pr*heightC
+// console.log(v)
+
+
+// let leg_n = 3
+// let leg_m = 4
+// let result = leg_n**2 + leg_m**2
+// let k = result**0.5
+// console.log(k)
+
+
