dbdaffe57814b898c97c388c554017b199ba2aba
hovvko <volodymyr.holovko13@gmail.com>
2021-12-10 19:45:46 +0200

    Homework and Classwork 11

---
+++ homework_11/favorites.js
@@ -0,0 +1,60 @@
+// let usersFavoriteArr = [];
+//
+// for (let i = 1; i < localStorage.length+1; i++) {
+//     usersFavoriteArr.push(JSON.parse(localStorage.getItem(i.toString())));
+// }
+//
+// for (let user of usersFavoriteArr) {
+//     let mainDiv = document.createElement("div");
+//     document.body.appendChild(mainDiv);
+//     mainDiv.style.display = "flex";
+//
+//     let pName = document.createElement("p");
+//     mainDiv.appendChild(pName);
+//     pName.innerText = (`Name: ${user.name}`);
+//     pName.classList.add("styleP");
+//
+//     let pAge = document.createElement("p");
+//     mainDiv.appendChild(pAge);
+//     pAge.innerText = (`Age: ${user.age}`);
+//     pAge.classList.add("styleP");
+//
+//     let pStatus = document.createElement("p");
+//     mainDiv.appendChild(pStatus);
+//     pStatus.innerText = (`Status: ${user.status}`);
+//     pStatus.classList.add("styleP");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
